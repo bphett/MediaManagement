62dd4d70b42827e5f5c1fb9f299628c4db4e480a
Jason Chaney <bphett@users.noreply.github.com>
2017-12-04 19:16:11 -0600

    various tweaks and additional install steps

--- home/convert_files.sh
+++ home/convert_files.sh
@@ -5,9 +5,11 @@ rm -rf /media/MediaServer/NewFiles/Temp
 
 moviedir="/media/MediaServer/Movies"
 tvdir="/media/MediaServer/TV Shows"
+transcodedir="/media/MediaServer/Transcode"
 runscript="/home/mp4_automator/manual.py"
 logfile="/media/MediaServer/MediaServerFiles/Logs/convertlog.txt"
 fileslist=/media/MediaServer/NewFiles/Temp/fileslist.txt
+runningfile="/media/MediaServer/MediaServerFiles/transcoding.txt"
 mkdir -p /media/MediaServer/NewFiles/Temp
 echo "::" > $fileslist
 
@@ -18,41 +20,65 @@ IFSold=$IFS
 
 #TV Shows section
 echo -e "\r" >> $logfile
-find '/media/MediaServer/TV Shows' -name '*.mkv' -printf '%p::' >> $fileslist
-find '/media/MediaServer/TV Shows' -name '*.avi' -printf '%p::' >> $fileslist
-find '/media/MediaServer/TV Shows' -name '*.wma' -printf '%p::' >> $fileslist
+find '/media/MediaServer/Transcode' -name '*.mkv' -printf '%p::' >> $fileslist
+find '/media/MediaServer/Transcode' -name '*.avi' -printf '%p::' >> $fileslist
+find '/media/MediaServer/Transcode' -name '*.wma' -printf '%p::' >> $fileslist
+find '/media/MediaServer/Transcode' -name '*.mp4' -printf '%p::' >> $fileslist
 files=$(cat "$fileslist")
+#echo "files=$files"
+echo ""
 
 IFS=$'::'
 
-for file in $files; do
-echo "\r" >> $logfile
-filename=$(basename "$file")
-filepath=$(echo $file | sed "s/\/$filename//")
-$runscript -i $file -m $filepath -a >> $logfile
-done
+if [ ! -e "$runningfile" ]; then
+echo "Transcoding" > $runningfile
+
+movepath="/media/MediaServer/NewFiles/Shows"
+$runscript -i $tvdir -m $movepath
+
+movepath="/media/MediaServer/NewFiles/Movies"
+$runscript -i $transcodedir -m $movepath
+
+rm -f $runningfile
+fi
+
+#for file in $files; do
+#if [ -z "$file" ]; then
+#   continue
+#fi
+#echo "\r" >> $logfile
+#filename=$(basename "$file")
+#filepath=$(echo $file | sed "s/\/$filename//")
+#movepath="/media/MediaServer/NewFiles/Shows"
+#echo "file=$file"
+##echo "filepath=$filepath"
+#echo $runscript
+#$runscript -i $tvdir -m $movepath
+## >> $logfile
+#done
 
 IFS=$IFSold
 
 #Movies section
-rm -rf $fileslist
-echo "::" > $fileslist
-echo -e "\r" >> $logfile
-find '/media/MediaServer/Movies' -name '*.mkv' -printf '%p::' >> $fileslist
-find '/media/MediaServer/Movies' -name '*.avi' -printf '%p::' >> $fileslist
-find '/media/MediaServer/Movies' -name '*.wma' -printf '%p::' >> $fileslist
-files=$(cat "$fileslist")
+#rm -rf $fileslist
+#echo "::" > $fileslist
+#echo -e "\r" >> $logfile
+#find '/media/MediaServer/Movies' -name '*.mkv' -printf '%p::' >> $fileslist
+#find '/media/MediaServer/Movies' -name '*.avi' -printf '%p::' >> $fileslist
+#find '/media/MediaServer/Movies' -name '*.wma' -printf '%p::' >> $fileslist
+#files=$(cat "$fileslist")
 
-IFS=$'::'
+#IFS=$'::'
 
-for file in $files; do
-echo "\r" >> $logfile
-filename=$(basename "$file")
-filepath=$(echo $file | sed "s/\/$filename//")
-$runscript -i $file -m $filepath -a >> $logfile
-done
+#for file in $files; do
 
-IFS=$IFSold
+#echo "\r" >> $logfile
+#filename=$(basename "$file")
+#filepath=$(echo $file | sed "s/\/$filename//")
+#$runscript -i $file -m $filepath -a >> $logfile
+#done
+
+#IFS=$IFSold
 
 #Test Lines
 #$runscript -i '/media/MediaServer/Test/testfile.mkv' -m '/media/MediaServer/Test/Result' -nd -a >> /media/MediaServer/Test/testlog.txt
